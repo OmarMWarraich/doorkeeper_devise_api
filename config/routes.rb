cf464cc72b95d6131c0a73d071cbe6638da17c41
OmarMWarraich <earthling986@gmail.com>
2023-03-20 15:58:10 +0500

    feat: Namespace Api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :books
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  namespace :api do
+    namespace :v1 do
+      resources :books
+    end
+  end
   # root "articles#index"
 end
